d02bde056e4ca44bc3e4ad7998bb9848f0d25b53
Anwesh43 <anweshthecool0@gmail.com>
2018-05-10 08:39:36 +0530

    adding IOView class

--- ioview/src/main/java/com/example/ioview/IOView.kt
+++ ioview/src/main/java/com/example/ioview/IOView.kt
@@ -3,3 +3,25 @@ package com.example.ioview
 /**
  * Created by anweshmishra on 10/05/18.
  */
+import android.content.*
+import android.graphics.*
+import android.view.*
+
+class IOView (ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+
+}
